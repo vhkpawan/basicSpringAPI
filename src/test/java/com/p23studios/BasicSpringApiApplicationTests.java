644ce760a48c7698726c903435930be803b051bb
vhkpawan <vhkpawan@gmail.com>
2025-09-14 20:10:02 -0400

    pushing basic refs

---
+++ src/test/java/com/p23studios/BasicSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p23studios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
